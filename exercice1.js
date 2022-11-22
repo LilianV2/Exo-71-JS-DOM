ae50cab2412f44e1c9c3f0c3a53ca4c135de86b7
lilia <lilianom59@outlook.fr>
2022-11-22 10:53:28 +0100

    dom

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,20 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+let monDiv2 = document.createElement('div');
+let viewP = document.getElementById('viewport');
 
 document.getElementById('viewport').appendChild(monDiv);
+
+monDiv.innerHTML = "Ce div a été ajouté en utilisant js";
+
+viewP.appendChild(monDiv2);
+
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = "100px";
+monDiv2.style.height = "100px";
+
+document.getElementById('aSupprimer').remove();
+
+
 
 
 
